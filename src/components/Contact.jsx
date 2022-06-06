17491481a6029cb5aaba7f83f86cfa2dfd0ecf4e
Aditya Ratna Shakya <ellitrym1@gmail.com>
2022-06-06 15:21:34 +0100

    Added contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,33 @@ export default class Contact extends Component {
             <div className="contact section">
                 <h5 className="title">CONTACT</h5>
                 <div className="contact-container">
-                    <form action=""></form>
+                    <form action="">
+                        <div className="form-row">
+                            <label htmlFor="contact-name">Name: </label>
+                            <input
+                                type="text"
+                                name="contact-name"
+                                id="contact-name"
+                            />
+                        </div>
+                        <div className="form-row">
+                            <label htmlFor="contact-email">Name: </label>
+                            <input
+                                type="text"
+                                name="contact-email"
+                                id="contact-email"
+                            />
+                        </div>
+                        <div className="form-row">
+                            <label htmlFor="contact-query">Name: </label>
+                            <textarea
+                                name="contact-query"
+                                id="contact-query"
+                                cols="30"
+                                rows="10"
+                            ></textarea>
+                        </div>
+                    </form>
                 </div>
             </div>
         );
